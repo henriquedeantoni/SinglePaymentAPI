0a10cbfbd0edb35920bbc7693b6affa9dd343721
henriquedeantoni <henriqdeantoni@gmail.com>
2025-04-04 00:19:26 -0300

    feat: transfermapper implementation

--- Mappers/TransferMapper.cs
+++ Mappers/TransferMapper.cs
@@ -1,7 +1,18 @@
-﻿namespace SinglePaymentAPI.Mappers;
+﻿using SinglePaymentAPI.Models;
+using SinglePaymentAPI.Models.DTOs;
 
-public static class TransferMapper : ITransferServices
+namespace SinglePaymentAPI.Mappers;
+
+public static class TransferMapper
 {
-
+    public static TransferDTO ToTransferDto(this TransferEntity transaction)
+    {
+        return new TransferDTO(
+            transaction.IdTransaction,
+            transaction.Sender,
+            transaction.Receiver,
+            transaction.Value
+        );
+    }
 }
 
